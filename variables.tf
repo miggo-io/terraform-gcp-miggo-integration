7a052c45413b0a3a3d6ee13b94b2ab52452cf581
Daniel Schafman <daniel.schafman@miggo.io>
2025-07-24 17:30:38 +0300

    chore(gcp-integrations): adding local variables

--- variables.tf
+++ variables.tf
@@ -7,20 +7,3 @@ variable "access_token" {
   description = "Miggo Access Token"
   type        = string
 }
-
-variable "aws_account_id" {
-  description = "AWS Account ID"
-  type        = string
-}
-
-variable "aws_role_name" {
-  description = "AWS Role name"
-  type        = string
-  default     = "gcp-integration"
-}
-
-variable "miggo_descope_project_id" {
-  description = "Miggo Descope Project ID"
-  type        = string
-}
-
